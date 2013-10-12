1662e0b6204d31649413c2c91484ef64f21912b3
Fernando Varella Lopes <fvarellalopes@gmail.com>
2013-10-12 17:37:04 -0300

    add web.xml e index.html

--- Solar/src/br/com/utfpr/solar/client/client/solar/SolarState.java
+++
@@ -1,8 +0,0 @@
-package br.com.utfpr.solar.client.client.solar;
-
-public class SolarState extends com.vaadin.shared.AbstractComponentState {
-
-	// TODO example state
-	public String text = "This is Solar";
-
-}
